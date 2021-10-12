a968213a7da9dc2c17fc6487ae7d4ad0e344d1e8
chenh <641760512@qq.com>
2021-10-12 16:26:41 +0800

    调试week4作业

---
+++ src/hello/week4/ch_4_homework1.java
@@ -0,0 +1,17 @@
+package hello.week4;
+
+import java.util.Scanner;
+
+public class ch_4_homework1 {
+	/*素数和
+	 * 给定两个整数n和m，0<n<=m<=200
+	 * 你的程序要计算第n个素数到第m个素数之间所有的素数的和，包括第n个素数和第m个素数。
+	 * 注意，是第n个素数到第m个素数之间的所有的素数，并不是n和m之间的所有的素数。
+	 * */
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt(); 
+
+	}
+
+}
---
+++ src/hello/week4/ch_4_homework2.java
@@ -0,0 +1,13 @@
+package hello.week4;
+
+import java.util.Scanner;
+
+public class ch_4_homework2 {
+	/*ÄîÕûÊı
+	 * */
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
