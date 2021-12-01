5256505bb3e5e3ab97d4a4a81a8a19ae1db8131e
chenh <641760512@qq.com>
2021-12-01 17:14:16 +0800

    异常处理：try-catch-finally

---
+++ src/com/ch/exceptionDemo/ExceptionTest2.java
@@ -0,0 +1,38 @@
+package com.ch.exceptionDemo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ExceptionTest2 {
+	@Test
+	public void test1() {
+		FileInputStream fis = null;
+		try {
+			File file = new File("hello.txt");
+			fis = new FileInputStream(file);
+
+			int data = fis.read();
+			while (data != -1) {
+				System.out.print((char) data);
+				data = fis.read();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
